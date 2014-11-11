0583f1d73c97d27b1afe89999c5e225e78049302
bigbug <bigbug@vpghqdpbigdata100>
2014-11-11 16:56:47 +0800

    add opensource tools

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,32 +1,14 @@
 package test;
 
-import java.io.File;
-import java.io.IOException;
-
 public class Test {
 
-	public static void main(String[] args) throws IOException {
-		File test = new File("/home/bigbug/workspace/AppCrawler/templates/template.sh");
-		System.out.println(test.getPath()); //  /home/bigbug/workspace/AppCrawler/templates/template.sh
-		System.out.println(test.getAbsolutePath());// 同上
-		System.out.println(test.getName()); //  template.sh
-		System.out.println(test.getParent()); //  /home/bigbug/workspace/AppCrawler/templates
-		
-		System.out.println("==========================");
-		test = new File("test/temp.txt");
-		System.out.println(test.getPath()); //  test/test.txt
-		System.out.println(test.getName()); //  test.txt
-		System.out.println(test.getParent()); //  test
-		System.out.println(test.getAbsoluteFile().getParent()); // /home/bigbug/workspace/PredictEngine/test
-		System.out.println(test.getAbsolutePath());// /home/bigbug/workspace/PredictEngine/test/test.txt
-		
-		
-//		File dir = new File("/home/bigbug/.android/avd");
-//		System.out.println(dir.getAbsolutePath());
-//		System.out.println(dir.getCanonicalPath());
-//		System.out.println(dir.getName());
-//		System.out.println(dir.getPath());
-		
+	public static void main(String[] args) throws ClassNotFoundException {
+//		Class c = Class.forName("String");
+		String c = "";
+		System.out.println(c.getClass());
+//		System.out.println(c.);
+//		System.out.println(c.getTypeName());
+//		System.out.println(c.);
 	}
 
 }
---
+++ src/test/TestFilenameFilepath.java
@@ -0,0 +1,32 @@
+package test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TestFilenameFilepath {
+
+	public static void main(String[] args) throws IOException {
+		File test = new File("/home/bigbug/workspace/AppCrawler/templates/template.sh");
+		System.out.println(test.getPath()); //  /home/bigbug/workspace/AppCrawler/templates/template.sh
+		System.out.println(test.getAbsolutePath());// 同上
+		System.out.println(test.getName()); //  template.sh
+		System.out.println(test.getParent()); //  /home/bigbug/workspace/AppCrawler/templates
+		
+		System.out.println("==========================");
+		test = new File("test/temp.txt");
+		System.out.println(test.getPath()); //  test/test.txt
+		System.out.println(test.getName()); //  test.txt
+		System.out.println(test.getParent()); //  test
+		System.out.println(test.getAbsoluteFile().getParent()); // /home/bigbug/workspace/PredictEngine/test
+		System.out.println(test.getAbsolutePath());// /home/bigbug/workspace/PredictEngine/test/test.txt
+		
+		
+//		File dir = new File("/home/bigbug/.android/avd");
+//		System.out.println(dir.getAbsolutePath());
+//		System.out.println(dir.getCanonicalPath());
+//		System.out.println(dir.getName());
+//		System.out.println(dir.getPath());
+		
+	}
+
+}
